8cc8daf4431a3ba368bc9e686a2916d60ea15d2b
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-08-11 23:52:14 -0700

    updates

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 
 # Create your views here.
@@ -10,10 +10,24 @@ def index(request):
 	return HttpResponse("Start Here")
 
 
-def create(request, words):
-	return HttpResponse("New blogs" )
+def new(request):
+	return HttpResponse("New blog")
+
+
+def create(request, nblogs):
+	return redirect('/blogs')
 
 
 
-def show(request, name):
-	return HttpResponse("Create new blog" + nblogs)
+def show(request, bnum):
+	print bnum
+	return HttpResponse("hello" + bnum)
+
+
+def edit(request, enum):
+	return HttpResponse("Edit this number" + enum)
+
+
+
+def destory(request, dnum):
+	return redirect('/blogs')
